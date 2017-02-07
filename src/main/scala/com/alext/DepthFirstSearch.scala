e34dad90a26be3beade3a684d7c5cbb086f0ffc5
oleksandr.tkachenko <alex.val.tkachenko@gmail.com>
2017-02-07 18:09:01 +0200

    Optimizing depth-first search

--- src/main/scala/com/alext/DepthFirstSearch.scala
+++ src/main/scala/com/alext/DepthFirstSearch.scala
@@ -3,8 +3,6 @@ package com.alext
 /**
   * Searches for an optimal solution using recursion.
   * The algorithm has exponential time complexity
-  * It also consumes stack memory and should be run with reasonable -Xss option
-  * e.g. -Xss32m for N=11138
   **/
 object DepthFirstSearch {
 
@@ -13,21 +11,31 @@ object DepthFirstSearch {
   }
 
   def search(state: State, goal: Int, iteration: Int, bestResult: Result): Result = {
-    if ((state.x > goal && state.y > goal) || (bestResult.isSolved && iteration >= bestResult.iterations))
+    if (state.x > goal || state.y > goal || (bestResult.isSolved && iteration >= bestResult.iterations))
       return bestResult
 
     if (state.x == goal || state.y == goal)
       return Result(true, iteration, state)
 
-    val resultOfX = search(state.OperationX(), goal, iteration + 1, bestResult)
+    if (iteration % 2 == 0) {
+      val resultOfX = search(state.OperationX(), goal, iteration + 1, bestResult)
 
-    search(state.OperationY(), goal, iteration + 1, resultOfX)
+      search(state.OperationY(), goal, iteration + 1, resultOfX)
+    } else {
+      val resultOfY = search(state.OperationY(), goal, iteration + 1, bestResult)
+
+      search(state.OperationX(), goal, iteration + 1, resultOfY)
+    }
   }
 
-
   def main(args: Array[String]): Unit = {
+    val startTime = System.currentTimeMillis()
     val initial = State(1, 1, null)
-
-    println(search(initial, 1138, 0, Result(false, 0, null)))
+    for (i <- 3 to 1000) {
+      val result = search(initial, i, 0, Result(false, 0, null))
+      println(i, result.iterations, Math.min(result.finalState.x, result.finalState.y))
+    }
+    val endTime = System.currentTimeMillis()
+    println("Time=" + (endTime - startTime))
   }
 }
