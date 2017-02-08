6671e24a008bd34ffc0cf65942ebd0382e503836
Alex Tkachenko <alex.val.tkachenko@gmail.com>
2017-02-08 21:09:14 +0200

    Optimizing starting point of search putting it in the middle

--- src/main/scala/com/alext/SingleDimensionScan.scala
+++ src/main/scala/com/alext/SingleDimensionScan.scala
@@ -8,7 +8,13 @@ object SingleDimensionScan {
   def search(n: Int): Result = {
     var bestResult = Result(false, n, null)
 
-    for (i <- (n - 1) to 1 by -1) {
+    for (i <- n / 2 to n - 1) {
+      val result = iterateBackwards(n, i, bestResult.iterations)
+      if (result.isSolved && (bestResult == null || result.iterations < bestResult.iterations))
+        bestResult = result
+    }
+
+    for (i <- n / 2 - 1 to 1 by -1) {
       val result = iterateBackwards(n, i, bestResult.iterations)
       if (result.isSolved && (bestResult == null || result.iterations < bestResult.iterations))
         bestResult = result
